2bf0bfb83824095379b33c1e399e162a68521630
won_chan <83020544+boychan112@users.noreply.github.com>
2021-08-05 23:25:31 +0900

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -50,7 +50,12 @@ async def on_message(message):
         embed=discord.Embed(description="https://lolchess.gg/builder/set5.5?deck=042b86f0e9ea11ebb5e589e115fb8433", color=0x00ff56)
         embed.set_author(name="<<4괴생명체  3망령  4주문술사  2싸움꾼>>",url="https://lolchess.gg/builder/set5.5?deck=042b86f0e9ea11ebb5e589e115fb8433")
         await message.channel.send(embed=embed)
-
+    
+    if content.startswith("!재생술사"):
+        embed=discord.Embed(description="https://lolchess.gg/builder/set5.5?deck=042b86f0e9ea11ebb5e589e115fb8433", color=0x00ff56)
+        embed.set_author(name="<<4재생술사 2기원자 2빛도자 2악동 3감시자 3>>",url="https://lolchess.gg/builder/set5.5?deck=042b86f0e9ea11ebb5e589e115fb8433")
+        await message.channel.send(embed=embed)
+        
     if(message.content == "!시간"):
         await message.channel.send(embed=discord.Embed(title="Time", timestamp=datetime.datetime.utcnow()))
 
